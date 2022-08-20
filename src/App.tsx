06c9c742884b3d24ac434b482727a1e4c2adc09a
Denny Ribeiro <denny.ribeiro@outlook.com>
2022-08-20 20:52:06 -0300

    <chore> creating router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import Router from "./Routes";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Teste</div>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
